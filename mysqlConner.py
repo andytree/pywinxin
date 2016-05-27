c30fec7c55c027910025280da587850f712c519e
bigtree <632622435@qq.com>
2016-05-27 17:30:42 +0800

    POST modify

--- mysqlConner.py
+++ mysqlConner.py
@@ -15,6 +15,7 @@ cursor.execute("select * from user ")
 results = cursor.fetchall()
 data = ''
 for row in results:
-	data = data + "id = "+ str(row[0]) + "姓名 = "+ row[1] + ",部门 = "+ str(row[2]) 
+	#data = data + "id = "+ str(row[0]) + "姓名 = "+ row[1] + ",部门 = "+ str(row[2]) 
+	data = data + row[1]
 
 mdb.close()
