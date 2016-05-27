c231d1c18348e28c551b167ade97f46ad595e4d1
bigtree <632622435@qq.com>
2016-05-27 16:46:07 +0800

    get mysql pas

--- mysqlConner.py
+++ mysqlConner.py
@@ -1,11 +1,11 @@
 import sae.const
 
-'''sae.const.MYSQL_DB      # 数据库名
+"""sae.const.MYSQL_DB      # 数据库名
 sae.const.MYSQL_USER    # 用户名
 sae.const.MYSQL_PASS    # 密码
 sae.const.MYSQL_HOST    # 主库域名（可读写）
 sae.const.MYSQL_PORT    # 端口，类型为<type 'str'>，请根据框架要求自行转换为int
-sae.const.MYSQL_HOST_S  # 从库域名（只读）'''
+sae.const.MYSQL_HOST_S  # 从库域名（只读）"""
 
 db = MySQLdb.connet(sae.const.MYSQL_HOST,sae.const.MYSQL_USER,
 	sae.const.MYSQL_PASS,sae.const.MYSQL_DB)
