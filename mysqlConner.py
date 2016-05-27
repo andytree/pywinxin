7001ec0a6c026d1ebe6fc17c94c8140d992bcd3f
bigtree <632622435@qq.com>
2016-05-27 16:56:58 +0800

    version modify

--- mysqlConner.py
+++ mysqlConner.py
@@ -8,7 +8,7 @@ sae.const.MYSQL_HOST    # 主库域名（可读写）
 sae.const.MYSQL_PORT    # 端口，类型为<type 'str'>，请根据框架要求自行转换为int
 sae.const.MYSQL_HOST_S  # 从库域名（只读）"""
 
-db = MySQLdb.connect(sae.const.MYSQL_HOST,sae.const.MYSQL_USER,
+db = MySQLdb.connect(sae.const.MYSQL_HOST,port = int(sae.const.MYSQL_PORT),sae.const.MYSQL_USER,
 	sae.const.MYSQL_PASS,sae.const.MYSQL_DB)
 cursor = db.cursor()
 cursor.execute("select version()")
