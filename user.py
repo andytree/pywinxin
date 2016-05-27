f44d3ce7f269dfa6073014a640aaafdfa5a4fab7
andytree <632622435@qq.com>
2016-05-28 00:05:09 +0800

    xx

---
+++ user.py
@@ -0,0 +1,7 @@
+class User(object):
+	"""员工的类，包括每个员工部门、职位、电话号码、短号、座机号等"""
+	def __init__(self, userName , departName , positionName ,
+		phoneNum , shortNum ,telNum):
+		super(User, self).__init__()
+		self.arg = arg
+		
