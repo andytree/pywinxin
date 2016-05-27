db65d2b3cc8e788c76edeb9bf4d6be2caa3df610
bigtree <632622435@qq.com>
2016-05-27 17:31:37 +0800

    POST gei

--- mysqlConner.py
+++ mysqlConner.py
@@ -16,6 +16,6 @@ results = cursor.fetchall()
 data = ''
 for row in results:
 	#data = data + "id = "+ str(row[0]) + "姓名 = "+ row[1] + ",部门 = "+ str(row[2]) 
-	data = data + row[1]
+	data = data + row[1].encode('utf-8')
 
 mdb.close()
