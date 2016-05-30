68d66350ec0d04b10b304f92ee1b082f31c400a0
bigtree <632622435@qq.com>
2016-05-30 11:31:59 +0800

    ddfd

--- mysqlConner.py
+++ mysqlConner.py
@@ -35,7 +35,8 @@ def getID(uname):
 		return True
 	for row in results:
 		#data = data + "id = "+ str(row[0]) + "姓名 = "+ row[1].decode('utf-8') + "部门 = "+ str(row[2]) 
-		user.userId ,newuser.userName ,newuser.departId = str(int(row[0])),row[1],str(int(row[2])
+		newuser.userId ,newuser.userName ,newuser.departId = str(int(row[0])),row[1],str(int(row[2])
+		print newuser.userId
 		return False
 
 #获取用户部门和上级部门
