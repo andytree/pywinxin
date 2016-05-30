8d4be86fb27142d660acc051de35c050580b26b3
bigtree <632622435@qq.com>
2016-05-30 15:12:21 +0800

    hhh

--- user.py
+++ user.py
@@ -14,11 +14,12 @@ class User(object):
 		userInfo = u'xxx'
 		print self.department,self.position
 		if self.departId not in ['0','1']:
-			userInfo = '欢迎使用联系人查询功能！\n姓名：' + self.userName
-			+ '\n部门：' + self.parentdepartment +
-			'\n部室：' +  self.department + '\n职位：'+ self.position +
-			'\n手机：' + self.phone + 
-			'\n短号：' + self.short +'\n机号：' + self.tel
+			userInfo = u'欢迎使用联系人查询功能！\n姓名：' + unicode(self.userName, 'utf-8')
+			+ u'\n部门：' + unicode(self.parentdepartment, 'utf-8')
+			'''+
+			u'\n部室：' +  self.department + u'\n职位：'+ self.position +
+			u'\n手机：' + self.phone + 
+			u'\n短号：' + self.short +u'\n机号：' + self.tel'''
 		else :
 			userInfo = u'欢迎使用联系人查询功能！\n姓名：' + self.userName
 			+ u'\n部门：' + self.department +
