ff4a57e702ff9a5078594abed037b381ad29add0
bigtree <632622435@qq.com>
2016-05-30 11:06:03 +0800

    'hah'

--- mysqlConner.py
+++ mysqlConner.py
@@ -44,7 +44,7 @@ def getDepartInfo():
 		newuser.department = results[0][0]
 	else :
 		results = cursor.execute("select a.depart_name as '部室',b.depart_name as '部门' from department as a, department as b where b.id = a.parent_id and a.id = " + newuser.departId)
-		print type(results[0][0])
+		type(results[0][0])
 		newuser.department = results[0][0]
 		newuser.parentdepartment = results[0][1]
 	return
