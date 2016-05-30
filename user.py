2f9d061d874cb3c48dfe856b6fb63ee67ccc7583
bigtree <632622435@qq.com>
2016-05-30 13:21:43 +0800

    n

--- user.py
+++ user.py
@@ -18,7 +18,7 @@ class User(object):
 			+ '\n部门：' + self.parentdepartment +
 			'\n部室：' +  self.department + '\n职位：' + self.position +
 			'\n手机：' + self.phone + 
-			'\n短号：' + self.short +'\n机号：' + self.tel).decode('utf-8')
+			'\n短号：' + self.short +'\n机号：' + self.tel)
 		else :
 			userInfo = u'欢迎使用联系人查询功能！\n姓名：' + self.userName
 			"""+ u'\n部门：' + self.department +
