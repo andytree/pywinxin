f2c771228a11b157c5181b58442805deef57df49
bigtree <632622435@qq.com>
2016-05-30 15:48:33 +0800

    get all

--- user.py
+++ user.py
@@ -12,18 +12,18 @@ class User(object):
 	def getUserInfo(self):
 		#部门ID不是0和1的，那么就是二级部室
 		userInfo = u'xxx'
-		print self.department,self.position,self.parentdepartment
+		#print self.department,self.position,self.parentdepartment
 		if self.departId not in ['0','1']:
 			userInfo = u'欢迎使用联系人查询功能！\n姓名：' + self.userName+ u'\n部门：'\
 			+unicode(self.parentdepartment)+\
 			u'\n部室：' + unicode(self.department) + u'\n职位：'+ unicode(self.position) +\
 			u'\n手机：' + unicode(self.phone) + \
-			u'\n短号：' + unicode(self.short) +u'\n机号：' + unicode(self.tel)
+			u'\n短号：' + unicode(self.short) +u'\n座机号：' + unicode(self.tel)
 		else :
 			userInfo = u'欢迎使用联系人查询功能！\n姓名：' + self.userName+\
 			u'\n部门：' + unicode(self.department) + u'\n职位：'+ unicode(self.position) +\
 			u'\n手机：' + unicode(self.phone) + \
-			u'\n短号：' + unicode(self.short) +u'\n机号：' + unicode(self.tel)
+			u'\n短号：' + unicode(self.short) +u'\n座机号：' + unicode(self.tel)
 		print userInfo
 		return userInfo
 		
