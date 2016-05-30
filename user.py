2fd324309261b32c2eff2a13c1887753cc2a739d
bigtree <632622435@qq.com>
2016-05-30 10:54:44 +0800

    'hah'

--- user.py
+++ user.py
@@ -4,7 +4,6 @@ import os
 class User(object):
 	"""员工的类，包括每个员工识别ID、部门、职位、电话号码、短号、座机号等"""
 	def __init__(self ):
-		super(User, self).__init__()
 	
 	'''userId , departId , userName , department , parentdepartment , position , phone ,short ,tel'''
 	#获取员工信息的拼接字段
